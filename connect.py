f22dbf5b6a7638eff0d623f5506dd141c26a53a1
lxndrvn <i1990522@gmail.com>
2017-01-24 20:44:40 +0100

    database connection

---
+++ connect.py
@@ -0,0 +1,8 @@
+from peewee import PostgresqlDatabase
+
+def create_database():
+    with open("connection.txt", "r") as f:
+        data_lines = f.readlines()
+        db_name = data_lines[0].replace("\n", "").split(":", 1)[1]
+        username = data_lines[1].replace("\n", "").split(":", 1)[1]
+        return PostgresqlDatabase(db_name, user=username)
