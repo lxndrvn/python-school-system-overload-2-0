6a9bc3c0674cf64edb66a87c7891d6f1db307aa2
lxndrvn <i1990522@gmail.com>
2017-01-24 20:23:31 +0100

    update build

--- build.py
+++ build.py
@@ -1,7 +1,8 @@
 # This script can create the database tables based on your models
 
+from connect import create_database
 from models import *
 
 db.connect()
 # List the tables here what you want to create...
-db.create_tables([], safe=True)
+db.create_tables([Applicant, School, City], safe=True)
