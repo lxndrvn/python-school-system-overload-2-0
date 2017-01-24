3124ab89e8ebb973bffc0c837c30a45a6c6dbabd
lxndrvn <i1990522@gmail.com>
2017-01-24 20:40:33 +0100

    add functions to handling

--- new_applicants.py
+++ new_applicants.py
@@ -1,4 +1,15 @@
 # New applicants arrive into your project database by this script.
 # You can run it anytime to generate new data!
 
+import uuid
 from models import *
+
+def handle_new_applicants():
+    new_applicants = Applicant.select().where(Applicant.application_code == None)
+    for applicant in new_applicants:
+        id = generate_id()
+        school_for_applicant = Applicant.city.school
+        applicant.update(application_code=id, school=school_for_applicant)
+
+def generate_id():
+    return str(uuid.uuid4())
