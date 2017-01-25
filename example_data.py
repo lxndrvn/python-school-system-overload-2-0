07cf63e4c87f1705e15fef9fedf18b191a411b8f
lxndrvn <i1990522@gmail.com>
2017-01-25 11:12:37 +0100

    add school

--- example_data.py
+++ example_data.py
@@ -1,3 +1,19 @@
 # This script can generate example data for "City" and "InterviewSlot" models.
 
 from models import *
+School.create(location="Budapest")
+bpschool=School.select().where(School.location="Budapest")
+School.create(location="Miskolc")
+misischool=School.select().where(School.location="Miskolc")
+School.create(location="Krakow")
+krakowschool=School.select().where(School.location="Krakow")
+
+cities=[{"name"="Budapest","school"="bpschool"},
+        {"name"="Esztergom","school"="bpschool"},
+        {"name"="Miskolc","school"="misischool"},
+        {"name"="Eger","school"="misischool"},
+        {"name"="Krakow","school"="krakowschool"},
+        {"name"="Warsaw","school"="krakowschool"}]
+
+for city in cities:
+    City.create(city["name"],city["school"])
