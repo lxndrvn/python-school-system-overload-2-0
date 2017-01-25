34a32cbb81d3e83977c031ee606454e21d566313
deviloop <blikpatrik@gmail.com>
2017-01-25 19:36:27 +0100

    application form adequation for datatable insertion

--- new_applicants.py
+++ new_applicants.py
@@ -5,17 +5,20 @@ import uuid
 from models import *
 
 def new_application():
-	first_name=input("Name: ")
-	last_name=input("Last name: ")
+	print("Let us know Your information to Apply!")
+	first_name=input("Given Name: ")
+	last_name=input("Familiy Name: ")
+	gender=input("Gender: ")
+	email=input("Email address: ")
+	city=input("Home city: ")
+	Applicant.create(application_code="",first_name=first_name,last_name=last_name,gender=gender,email=email,city=City.select().where(City.name==city).get(),school=City.select().where(City.name==city).get().school,status="NEW")
 
-	Applicant.create(first_name,last_name)
+def generate_code():
+    return str(uuid.uuid4())
 
 def handle_new_applicants():
-    new_applicants = Applicant.select().where(Applicant.application_code == None)
+    new_applicants = Applicant.select().where(Applicant.status == "New")
     for applicant in new_applicants:
-        id = generate_id()
+        applicant.application_code = generate_code()
         school_for_applicant = Applicant.city.school
         applicant.update(application_code=id, school=school_for_applicant)
-
-def generate_id():
-    return str(uuid.uuid4())
