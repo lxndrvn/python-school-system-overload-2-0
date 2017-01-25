a991ae7c80c108af98b8123b9da01d9acf631d2f
deviloop <blikpatrik@gmail.com>
2017-01-25 19:23:23 +0100

    dropping tables on tables initiation added

--- build.py
+++ build.py
@@ -1,8 +1,8 @@
 # This script can create the database tables based on your models
-
 from connect import create_database
 from models import *
 
 db.connect()
+db.drop_tables([School, City, Applicant, Mentor, InterviewSlot, Interview])
 # List the tables here what you want to create...
 db.create_tables([School, City, Applicant, Mentor, InterviewSlot, Interview], safe=True)
